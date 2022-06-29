211df5c967444ffdb0e5f77439cf183af2f37937
Csaba Bartfai <csaba_bartfai@epam.com>
2022-06-30 00:21:55 +0200

    Remove stuff create some basic stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <div className="container md mx-auto bg-slate-500 h-screen">
+    <div className='flex flex-col h-screen place-content-evenly'>
+      <p className='text-center'>Hello mother!</p>
+      <iframe
+        className="mx-auto place-content-center"
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/HyWYpM_S-2c"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen></iframe>
+        <button className="p-2 bg-orange-200 text-center mx-auto">Look at me!</button>
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
